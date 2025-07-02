263eb3d83f57eeb6de54b28eb03146313bedebca
Nick Strupat <nick.strupat@gmail.com>
2025-07-01 20:33:20 -0400

    add type parameter constraints

--- IL-Lumination/Builders/TypeParameterBuilder.cs
+++ IL-Lumination/Builders/TypeParameterBuilder.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Illumination.Builders;
 
@@ -9,7 +11,16 @@ public sealed class TypeParameterBuilder
 	internal String? name { get; private set; }
 	public TypeParameterBuilder Name(String name) { this.name = name; return this; }
 	
-	internal TypeRef? constraint { get; private set; }
-	public TypeParameterBuilder Constraint(Type type) { constraint = type; return this; }
-	public TypeParameterBuilder Constraint(TypeBuilder typeBuilder) { constraint = typeBuilder; return this; }
+	internal TypeRef? baseType { get; private set; }
+	internal readonly List<TypeRef> interfaces = new();
+	internal GenericParameterAttributes variance { get; private set; }
+	internal GenericParameterAttributes special { get; private set; }
+	internal GenericParameterAttributes allowByRefLike { get; private set; }
+	public TypeParameterBuilder BaseTypeConstraint(Type type) { baseType = type; return this; }
+	public TypeParameterBuilder AddInterfaceConstraint(Type type) { interfaces.Add(type); return this; }
+	public TypeParameterBuilder Invariant() { variance = GenericParameterAttributes.None; return this; }
+	public TypeParameterBuilder Covariant() { variance = GenericParameterAttributes.Covariant; return this; }
+	public TypeParameterBuilder Contravariant() { variance = GenericParameterAttributes.Contravariant; return this; }
+	public TypeParameterBuilder Constraint(GenericParameterAttributes attributes) { special |= attributes; return this; }
+	public TypeParameterBuilder AllowByRefLike() { allowByRefLike = GenericParameterAttributes.AllowByRefLike; return this; }
 }
