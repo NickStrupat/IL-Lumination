74f0cc9b3eff43a6de78b695347375ecddfb440e
Nick Strupat <nick.strupat@gmail.com>
2025-10-04 00:14:25 -0400

    Revert "refactor: simplify method overloads and add extension methods for AssemblyBuilder, EnumBuilder, MethodBuilder, and TypeBuilder"
    This reverts commit 9c7d15df39a7759ddb2b62690dbb0e14b19dd0b0.

--- IL-Lumination/Builders/MethodBuilder.cs
+++ IL-Lumination/Builders/MethodBuilder.cs
@@ -34,17 +34,27 @@ public abstract class MethodBuilder<T> : MethodBuilder where T : MethodBuilder<T
 
 	public T NewParameter(out ParameterBuilder parameterBuilder, Action<ParameterBuilder> parameterBuilderAction) =>
 		(T)this.AddAction(parameters.AsContravariant(), parameterBuilder = new((Int16)parameters.Count), parameterBuilderAction);
-
+	public T NewParameter(out ParameterBuilder parameterBuilder) => NewParameter(out parameterBuilder, _ => {});
+	public T NewParameter(Action<ParameterBuilder> parameterBuilderAction) => NewParameter(out _, parameterBuilderAction);
+	
 	public T NewParameter<TParam>(out ParameterBuilder<TParam> parameterBuilder, Action<ParameterBuilder<TParam>> parameterBuilderAction) =>
 		(T)this.AddAction(parameters.AsContravariant(), parameterBuilder = new((Int16)parameters.Count), parameterBuilderAction);
-
+	public T NewParameter<TParam>(out ParameterBuilder<TParam> parameterBuilder) => NewParameter(out parameterBuilder, _ => {});
+	public T NewParameter<TParam>(Action<ParameterBuilder<TParam>> parameterBuilderAction) => NewParameter(out _, parameterBuilderAction);
+	
 	public T NewTypeParameter(out TypeParameterBuilder typeParameterBuilder, Action<TypeParameterBuilder> typeParameterBuilderAction) =>
 		(T)this.AddAction(typeParameters, typeParameterBuilder = new(), typeParameterBuilderAction);
-
+	public T NewTypeParameter(out TypeParameterBuilder typeParameterBuilder) => NewTypeParameter(out typeParameterBuilder, _ => {});
+	public T NewTypeParameter(Action<TypeParameterBuilder> typeParameterBuilderAction) => NewTypeParameter(out _, typeParameterBuilderAction);
+	
 	public T NewLocal(out LocalBuilder localBuilder, Action<LocalBuilder> localBuilderAction) => (T)this.AddAction(locals.AsContravariant(), localBuilder = new((Int16)locals.Count), localBuilderAction);
-
+	public T NewLocal(out LocalBuilder localBuilder) => NewLocal(out localBuilder, _ => {});
+	public T NewLocal(Action<LocalBuilder> localBuilderAction) => NewLocal(out _, localBuilderAction);
+	
 	public T NewLocal<TLocal>(out LocalBuilder<TLocal> localBuilder, Action<LocalBuilder<TLocal>> localBuilderAction) => (T)this.AddAction(locals.AsContravariant(), localBuilder = new((Int16)locals.Count), localBuilderAction);
-
+	public T NewLocal<TLocal>(out LocalBuilder<TLocal> localBuilder) => NewLocal(out localBuilder, _ => {});
+	public T NewLocal<TLocal>(Action<LocalBuilder<TLocal>> localBuilderAction) => NewLocal(out _, localBuilderAction);
+	
 	public T Body(Action<BodyBuilder> bodyAction) { this.bodyActions.Add(bodyAction); return (T)this; }
 }
 
@@ -61,44 +71,16 @@ public sealed class NestedMethodBuilder : MethodBuilder<NestedMethodBuilder>
 {
 	internal NestedMethodBuilder(TypeBuilder containingType) : base(MethodAttributes.Private, default) => this.containingType = containingType;
 	private readonly TypeBuilder containingType;
-
+	
 	public NestedMethodBuilder Private() { this.visibility = MethodAttributes.Private; return this; }
 	public NestedMethodBuilder Family() { this.visibility = MethodAttributes.Family; return this; }
 	public NestedMethodBuilder FamilyAndAssembly() { this.visibility = MethodAttributes.FamANDAssem; return this; }
 	public NestedMethodBuilder FamilyOrAssembly() { this.visibility = MethodAttributes.FamORAssem; return this; }
 	public NestedMethodBuilder Assembly() { this.visibility = MethodAttributes.Assembly; return this; }
 	public NestedMethodBuilder Public() { this.visibility = MethodAttributes.Public; return this; }
-
+	
 	public NestedMethodBuilder Static() { this.storageType = MethodAttributes.Static; return this; }
 	public NestedMethodBuilder NonStatic() { this.storageType = default; return this; }
-
+	
 	public NestedMethodBuilder EntryPoint() { this.containingType.assemblyBuilder.entryPoint = this; return this; }
-}
-
-public static class MethodBuilderExtensions
-{
-	public static T NewParameter<T>(this MethodBuilder<T> mb, out ParameterBuilder parameterBuilder) where T : MethodBuilder<T> =>
-		mb.NewParameter(out parameterBuilder, _ => {});
-	public static T NewParameter<T>(this MethodBuilder<T> mb, Action<ParameterBuilder> parameterBuilderAction) where T : MethodBuilder<T> =>
-		mb.NewParameter(out _, parameterBuilderAction);
-
-	public static T NewParameter<T, TParam>(this MethodBuilder<T> mb, out ParameterBuilder<TParam> parameterBuilder) where T : MethodBuilder<T> =>
-		mb.NewParameter(out parameterBuilder, _ => {});
-	public static T NewParameter<T, TParam>(this MethodBuilder<T> mb, Action<ParameterBuilder<TParam>> parameterBuilderAction) where T : MethodBuilder<T> =>
-		mb.NewParameter(out _, parameterBuilderAction);
-
-	public static T NewTypeParameter<T>(this MethodBuilder<T> mb, out TypeParameterBuilder typeParameterBuilder) where T : MethodBuilder<T> =>
-		mb.NewTypeParameter(out typeParameterBuilder, _ => {});
-	public static T NewTypeParameter<T>(this MethodBuilder<T> mb, Action<TypeParameterBuilder> typeParameterBuilderAction) where T : MethodBuilder<T> =>
-		mb.NewTypeParameter(out _, typeParameterBuilderAction);
-
-	public static T NewLocal<T>(this MethodBuilder<T> mb, out LocalBuilder localBuilder) where T : MethodBuilder<T> =>
-		mb.NewLocal(out localBuilder, _ => {});
-	public static T NewLocal<T>(this MethodBuilder<T> mb, Action<LocalBuilder> localBuilderAction) where T : MethodBuilder<T> =>
-		mb.NewLocal(out _, localBuilderAction);
-
-	public static T NewLocal<T, TLocal>(this MethodBuilder<T> mb, out LocalBuilder<TLocal> localBuilder) where T : MethodBuilder<T> =>
-		mb.NewLocal(out localBuilder, _ => {});
-	public static T NewLocal<T, TLocal>(this MethodBuilder<T> mb, Action<LocalBuilder<TLocal>> localBuilderAction) where T : MethodBuilder<T> =>
-		mb.NewLocal(out _, localBuilderAction);
 }
