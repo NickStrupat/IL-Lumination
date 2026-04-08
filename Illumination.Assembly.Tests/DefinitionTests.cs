ba267ef2c3dd18ef755c156ff069bcb3625c6b15
Nick Strupat <nick.strupat@gmail.com>
2026-04-08 15:11:11 -0400

    upgrade nuget packages

--- Illumination.Assembly.Tests/DefinitionTests.cs
+++ Illumination.Assembly.Tests/DefinitionTests.cs
@@ -2,7 +2,6 @@ using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Xunit.Abstractions;
 
 namespace Illumination.Assembly.Tests;
 
